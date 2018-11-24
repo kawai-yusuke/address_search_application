9d0ae5851cc73fddb1d6016894ab50eb2bf662a0
Yusuke <homekawai22goal@yahoo.co.jp>
2018-11-24 17:12:09 +0900

    エラーが一つ減った

--- test_address_searcher.py
+++ test_address_searcher.py
@@ -1,9 +1,20 @@
 import unittest
+import requests
 
 
 class AdressSearcher():
     def search(self, postal_code):
-        return "岩手県八幡平市大更"
+        url = "http://zipcloud.ibsnet.co.jp/api/search?zipcode=0287111"
+        response = requests.get(url)
+        response_dict = response.json()
+
+        都道府県 = response_dict["results"][0]["address1"]
+        市区町村 = response_dict["results"][0]["address2"]
+        町域 = response_dict["results"][0]["address3"]
+        return f"{都道府県}{市区町村}{町域}"
+
+
+
 
 class TestAdressSearcher(unittest.TestCase):
     def test_岩手県八幡平市大更を郵便番号から取得できる(self):
@@ -19,7 +30,6 @@ class TestAdressSearcher(unittest.TestCase):
         actual = address_seaecher.search(postal_code="1760014")
 
         self.assertEqual("東京都練馬区豊玉南", actual)
-
 
 
 if __name__ == "__main__":
